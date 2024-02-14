c57ca69520ce0ae6b5f69eba7b9d2990a5f88565
rinagoldberg13@gmail.com <rinagoldberg13@gmail.com>
2024-02-14 12:56:48 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -14,10 +14,14 @@ class Form1(Form1Template):
   def button_1_click(self, **event_args):
     # Get the text input from the text box
     text_input = self.name.text
-    result = anvil.server.call('main', text_input)
+    result = anvil.server.call('name', text_input)
     if result:
         self.result.visible = True 
         self.result.text = result.capitalize()
+
+  def file_loader_2_change(self, file, **event_args):
+    """This method is called when a new file is loaded into this FileLoader"""
+    result, score = anvil.server.call('classify_image', file)
   
 
 
