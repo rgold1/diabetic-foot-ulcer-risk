1fdbe42c6e13ed36e13044358837af19852e2ec1
rinagoldberg13@gmail.com <rinagoldberg13@gmail.com>
2024-02-15 10:33:09 +0000

    Created DiabeticLogo.jpg; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,7 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
-
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
